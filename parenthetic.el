42927e5f5b85fafbec04352bc4bd84ce76bec2e1
Jens C. Jensen <jens@subst.net>
2021-04-18 11:43:24 +0200

    initial commit

---
+++ parenthetic.el
@@ -0,0 +1,21 @@
+;;; parenthetic.el --- Some description. -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021 Jens C. Jensen
+
+;; Author: Jens C. Jensen <jens@subst.net>
+;; URL:
+;; Keywords:
+;; Package-Requires ((emacs "28.0.50"))
+;; Package-Version: 20210418
+;; Version: 0.1.0
+
+
+;;; Commentary:
+;;
+
+;;; Code:
+
+
+
+(provide 'parenthetic)
+;;; parenthetic.el/parenthetic.el ends here
